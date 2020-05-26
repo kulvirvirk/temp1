34727974e490df2823ae190b426df9b220dccece
null null <kvirkdev@gmail.com>
2020-05-26 05:02:15 +0000

    deleted other temp file

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print (os.getcwd())
+x = 5
+y = 5
+z = x + y
 
-x = 5 
-y = 10 
+# length = input("Enter room's length: ")
+# width = input("Enter room's width: ")
+
+# area  = int(length) * int(width)
+# print("Room's area = " + str(area))
